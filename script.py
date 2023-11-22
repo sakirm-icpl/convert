30451e3d5788661d1f5af436e87b2a8089e79f29
Sakir Malek <sakirm@infopercept.com>
2023-11-22 16:27:46 +0530

    Update script.py
    New code added

--- script.py
+++ script.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import json
-
-# Load JSON file
-with open('/var/lib/jenkins/workspace/MobSF/Reference.json') as f:
-    data = json.load(f)
-
-# Convert to DataFrame
+import os
+ 
+# Specify the JSON file name
+json_file_name = "/var/lib/jenkins/workspace/test/insecure-bank-Reference.json"
+ 
+# Extract the base name of the JSON file (without extension)
+base_name = os.path.splitext(os.path.basename(json_file_name))[0]
+ 
+# Load JSON data from file
+with open(json_file_name, 'r') as json_file:
+    data = json.load(json_file)
+ 
+# Convert JSON to DataFrame
 df = pd.json_normalize(data)
-
-# Save to Excel
-df.to_excel('/var/lib/jenkins/workspace/mobsf/Reference.xlsx', index=False)
+ 
+# Save DataFrame to Excel with the same base name as JSON file
+excel_file_name = f'{base_name}.xlsx'
+df.to_excel(excel_file_name, index=False)
+ 
+print(f"Conversion completed. Excel file saved as: {excel_file_name}")
